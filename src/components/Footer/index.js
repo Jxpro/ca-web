465c0b90796e11964d6c00e2c15516e88e19afcf
jokerxin <jxpro@qq.com>
2022-12-17 21:20:18 +0800

    Complete the Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import { Layout } from 'antd';
+import './index.css';
+
+const Footer = () =>
+    <Layout.Footer className="footer">
+        Ant Design ©2022 Created by Joker Xin
+    </Layout.Footer>;
+
+export default Footer;
