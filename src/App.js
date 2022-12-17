c0e2f80c35533412ce3e823b444218e09311dfba
jokerxin <jxpro@qq.com>
2022-12-17 21:19:07 +0800

    Set up basic framework  in the App component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { Breadcrumb, Layout } from 'antd';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
+const { Content } = Layout;
+
 function App() {
     return (
-        <div className="App">
-            This is certification authrority build with ReactJS
-        </div>
+        <Layout>
+            <Header />
+            <Content className="site-layout">
+                <div className="site-layout-background">
+                    Content
+                </div>
+            </Content>
+            <Footer />
+        </Layout>
     );
 }
 
