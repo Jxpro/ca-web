1ffcb6860ac9ff58dcfbb7f6363c7d056c60d30d
jokerxin <jxpro@qq.com>
2022-12-18 17:07:32 +0800

    Update the Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,53 @@
 import { useState } from 'react';
-import { Button, Layout, Menu, Modal, Checkbox, Form, Input, message } from 'antd';
-import { LoadingOutlined, LockOutlined, CheckCircleOutlined, WarningOutlined, SafetyCertificateOutlined, FormOutlined, EyeOutlined, FileProtectOutlined, QuestionCircleOutlined, UserOutlined, LoginOutlined } from '@ant-design/icons';
+import { Button, Layout, Menu, Modal, Form, Input, message } from 'antd';
+import { IdcardOutlined, LoadingOutlined, LockOutlined, CheckCircleOutlined, WarningOutlined, SafetyCertificateOutlined, FormOutlined, EyeOutlined, FileProtectOutlined, QuestionCircleOutlined, UserOutlined } from '@ant-design/icons';
 import api from '../../api';
 import './index.css';
 
-function Header() {
-    // 获取用户信息
-    const [userInfo, setUserInfo] = useState(undefined);
-    // 从后端获取用户信息
-    // api.user.getUserInfo().then(res => {
-    //     setUserInfo(res);
-    // }, err => {
-    //     console.log(err);
-    // });
+// 接收一个中英文字符串，中文算2个字符，英文算1个字符
+// 最后返回一个长度为6个字符的字符串加上...，
+// 如果字符串长度小于6个字符则不加...
+function getShortName(name) {
+    let shortName = '';
+    let count = 0;
+    for (let i = 0; i < name.length; i++) {
+        if (name.charCodeAt(i) > 255) {
+            count += 2;
+        } else {
+            count += 1;
+        }
+        if (count > 6) {
+            break;
+        }
+        shortName += name[i];
+    }
+    if (count > 6) {
+        shortName += '...';
+    }
+    return shortName;
+}
 
+function Header() {
+    // 从后端获取用户信息, useRef防止重复请求, function(){...}()是为了立即执行函数
+    // deprecated: 由于useRef传入立即执行的函数，还是会导致每次渲染都会执行函数，因此改为useState
+    // let userInfo = useRef(function () {
+    //     // api.user.getUserInfo().then(res => {
+    //     //     return res;
+    //     // }, err => {
+    //     //     return undefined;
+    //     // });
+    //     console.log('get user info');
+    //     return undefined;
+    // }());
+    const [userInfo, setUerInfo] = useState(() => {
+        // api.user.getUserInfo().then(res => {
+        //     return res;
+        // }, err => {
+        //     return undefined;
+        // });
+        console.log('get user info');
+        return { name: 'admmmmmmmmmmmmm', role: 'admin' };
+    });
 
     // 导航项
     // 将true or false改为字符串形式，否则react会警告：Received `true` for a non-boolean attribute `show`.
@@ -46,7 +80,7 @@ function Header() {
             label: '证书审核',
             key: 'certCheck',
             icon: <EyeOutlined />,
-            show: 'true',
+            show: userInfo && userInfo.role === 'admin' ? 'true' : 'false',
             // TODO: 需要判断用户是否为管理员
         },
         {
@@ -74,19 +108,16 @@ function Header() {
     const [messageApi, contextHolder] = message.useMessage();
     // 控制登录还是注册
     const [isLogin, setLoginMode] = useState(false);
-    // 控制登录或注册的跳转按钮
-    const [disableLogin, setDisableLogin] = useState(false);
-    const [disableRegister, setDisableRegister] = useState(false);
     // 控制登录注册弹窗
     const [openModal, setOpenModal] = useState(false);
     const [confirmLoading, setConfirmLoading] = useState(false);
     // 提交登录表单
-    const onFinishLogin = values => {
+    const onLogin = values => {
         console.log('Received values of form: ', values);
         setConfirmLoading(true);
-        setDisableRegister(true);
         // api.user.login(values).then(res => {
         //     console.log(res);
+        //     setUerInfo(res);
         //     setConfirmLoading(false);
         //     setOpenModal(false);
         //     messageApi.open({
@@ -103,12 +134,12 @@ function Header() {
         // });
     };
     // 提交注册表单
-    const onFinishRgister = values => {
+    const onRgister = values => {
         console.log('Received values of form: ', values);
         setConfirmLoading(true);
-        setDisableLogin(true);
         // api.user.register(values).then(res => {
         //     console.log(res);
+        //     setUerInfo(res);
         //     setConfirmLoading(false);
         //     setOpenModal(false);
         //     messageApi.open({
@@ -122,6 +153,23 @@ function Header() {
         //         type: 'error',
         //         content: '注册失败',
         //     });
+        // });
+    };
+    // 退出登录
+    const onLogout = () => {
+        // 删除本地存储的token
+        localStorage.removeItem('token');
+        // 重置用户信息
+        setUerInfo(undefined);
+        // 提示退出成功
+        messageApi.open({
+            type: 'success',
+            content: '退出成功',
+        });
+        // 发送退出请求，让后端删除token
+        // 未删除的话也不影响，因为本地已无token，无法通过验证
+        // api.user.logout().then(res => {
+        //     console.log(res);
         // });
     };
     // 取消弹窗
@@ -153,11 +201,15 @@ function Header() {
                 <div className="content">
                     <UserOutlined />
                     &nbsp;&nbsp;
-                    {userInfo ? <span>userInfo.name</span> : <>
-                        <Button type="primary" onClick={onClickLogin}>登录</Button>
-                        &nbsp;
-                        <Button onClick={onClickRegist}>注册</Button>
-                    </>}
+                    {userInfo
+                        ? <>
+                            <span>{getShortName(userInfo.name)}</span>&nbsp;&nbsp;
+                            <Button type="primary" danger onClick={onLogout}>登出</Button>
+                        </>
+                        : <>
+                            <Button type="primary" onClick={onClickLogin}>登录</Button>&nbsp;&nbsp;
+                            <Button onClick={onClickRegist}>注册</Button>
+                        </>}
                 </div>
             </div>
             <Menu
@@ -181,8 +233,21 @@ function Header() {
                 <Form
                     name="normal_login"
                     className="login-form"
-                    onFinish={isLogin ? onFinishLogin : onFinishRgister}
-                >
+                    onFinish={isLogin ? onLogin : onRgister}
+                >{!isLogin
+                    && <Form.Item
+                        name="nickname"
+                        rules={[
+                            {
+                                required: true,
+                                message: '请输入昵称!',
+                            },
+                        ]}
+                    >
+                        <Input prefix={<IdcardOutlined className="site-form-item-icon" />}
+                            placeholder="Nickname" />
+                    </Form.Item>
+                    }
                     <Form.Item
                         name="username"
                         rules={[
@@ -192,7 +257,8 @@ function Header() {
                             },
                         ]}
                     >
-                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />}
+                            placeholder="Username" />
                     </Form.Item>
                     <Form.Item
                         name="password"
@@ -217,7 +283,7 @@ function Header() {
                             rules={[
                                 {
                                     required: true,
-                                    message: 'Please confirm your password!',
+                                    message: '两次密码输入不一致!',
                                 },
                                 ({ getFieldValue }) => ({
                                     validator(_, value) {
@@ -242,7 +308,7 @@ function Header() {
                         &nbsp;&nbsp;&nbsp;&nbsp;
                         <Button className="login-form-button"
                             onClick={isLogin ? onClickRegist : onClickLogin}
-                            disabled={isLogin ? disableRegister : disableLogin}>
+                            disabled={confirmLoading}>
                             {isLogin ? '还没账号，去注册' : '已有账号，去登录'}
                         </Button>
                     </Form.Item>
