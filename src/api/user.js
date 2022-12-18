c1a17b7e167d443e287e1cc454224120fef8f500
jokerxin <jxpro@qq.com>
2022-12-18 19:14:42 +0800

    Ensure security by hashing password

--- src/api/user.js
+++ src/api/user.js
@@ -1,3 +1,5 @@
+import { sha256 } from 'js-sha256';
+
 import http from './http';
 
 const api_prefix = '/user';
@@ -5,11 +7,20 @@ const api_prefix = '/user';
 export default {
     // 登录
     login(data) {
-        return http.post(api_prefix + '/login', data);
+        const { password } = data;
+        return http.post(api_prefix + '/login', {
+            ...data,
+            password: sha256(password),
+        });
     },
     // 注册
     register(data) {
-        return http.post(api_prefix + '/register', data);
+        delete data.confirm;
+        const { password } = data;
+        return http.post(api_prefix + '/register', {
+            ...data,
+            password: sha256(password),
+        });
     },
     // 退出登录
     logout() {
