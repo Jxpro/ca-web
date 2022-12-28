56d14c148abb19f232bde6197d296dcd0dd67702
jokerxin <jxpro@qq.com>
2022-12-29 00:53:30 +0800

    Update cert api

--- src/api/cert.js
+++ src/api/cert.js
@@ -6,10 +6,25 @@ export default {
     list(state) {
         return http.get(`${api_prefix}/${state}`);
     },
+    uncompletedCert() {
+        return http.get(`${api_prefix}/user/uncompleted`);
+    },
+    applySubject(subject) {
+        return http.post(`${api_prefix}/apply/subject`, subject);
+    },
+    applyPubKey(pubKey) {
+        return http.post(`${api_prefix}/apply/publicKey`, pubKey);
+    },
+    applyLicense(license) {
+        return http.post(`${api_prefix}/apply/license`, license);
+    },
     accept(requestId) {
         return http.post(`${api_prefix}/approve`, { id: requestId, passed: true });
     },
     reject(requestId) {
         return http.post(`${api_prefix}/approve`, { id: requestId, passed: false });
+    },
+    revoke(requestId) {
+        return http.post(`${api_prefix}/revoke`, { id: requestId });
     },
 };
