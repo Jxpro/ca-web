639d811a821d5b8e28d356f13871b1a00ed48680
jokerxin <jxpro@qq.com>
2022-12-29 00:53:50 +0800

    Update file api

--- src/api/file.js
+++ src/api/file.js
@@ -3,11 +3,14 @@ import http from './http';
 const api_prefix = '/file';
 
 export default {
-    downloadCRL() {
-        return http.get(`${api_prefix}/crl`, { responseType: 'blob' });
+    downloadRootCert() {
+        return http.get(`${api_prefix}/cert/root`, { responseType: 'blob' });
     },
     downloadCert(requestId) {
         return http.get(`${api_prefix}/cert/${requestId}`, { responseType: 'blob' });
+    },
+    downloadCRL() {
+        return http.get(`${api_prefix}/crl`, { responseType: 'blob' });
     },
     downloadLicense(contentHash) {
         return http.get(`${api_prefix}/license/${contentHash}`, { responseType: 'blob' });
