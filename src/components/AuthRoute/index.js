46453040d218b04fb063d9705843d0a9fac1de1a
jokerxin <jxpro@qq.com>
2022-12-30 13:06:11 +0800

    Fix incorrect judgment of admin in route guard

--- src/components/AuthRoute/index.js
+++ src/components/AuthRoute/index.js
@@ -9,8 +9,12 @@ function AuthRoute(props) {
     if (!token) {
         return <Navigate to="/login" replace state={{ errorMsg: '请先登录', from: pathname }} />;
     }
+    const userinfo = token.split('.')[1];
+    const { role } = JSON.parse(decodeURIComponent(window.atob(userinfo)));
     if (requireAdmin) {
-        api.user.info().then(res => res.role === 'admin' && (isAdmin = true));
+        // 发一次请求给后端，防止token被篡改，如果token被篡改，则后端会返回401，然后前端会清除token
+        api.user.info();
+        isAdmin = role === 'admin' ? true : false;
         return isAdmin ? children : <Navigate to="/" replace state={{ errorMsg: '权限不足' }} />;
     }
     return children;
